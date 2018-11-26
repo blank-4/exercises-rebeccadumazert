a6efc912bb4ad93e82d51faa98e1bbcc3f8c4f26
Rebecca Dumazert <rebeccadumazert@hotmail.fr>
2018-11-26 17:04:33 +0100

    exo2

---
+++ javascript/lesson2/exercise2/exercise2/main.js
@@ -0,0 +1,20 @@
+/* Créez un compteur.
+
+* Il doit compter de 0 à 100. (afficher le nombre à chacune des itérations).
+* Maintenant, transformez le pour qu'il compte de deux en deux.
+* Inversez le pour qu'il décompte de 2 en 2 de 100 à 0. */
+
+for (var i = 0 ; i <= 100 ; i++) {
+    console.log("je compte jusqu'à : " + i);
+    
+}
+
+for (var i = 0; i < 100 ; i=i + 2) {
+    console.log("je compte jusqu'à :" + i);
+    
+}
+
+for (var i = 100; i > 0 ; i=i - 2) {
+    console.log("je compte jusqu'à :" + i);
+    
+}
