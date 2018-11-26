f15ed887599a5a53a3a3b1a81036fc1c3e729718
Rebecca Dumazert <rebeccadumazert@hotmail.fr>
2018-11-26 12:12:51 +0100

    exercice2+correction1

--- javascript/lesson1/exercise1/exercice/main.js
+++ javascript/lesson1/exercise1/exercice/main.js
@@ -1,17 +1,17 @@
 var toto="bonjour";
 console.log(toto);
 
-toto = 1 + 1; 
+toto = 1+1; 
 console.log(toto); 
 
-toto= 5 - 7;
+toto = 5-7;
 console.log(toto);
 
-toto= 10 * 10;
+toto = 10*10;
 console.log(toto);
 
-toto= 10/2;
+toto = 10/2;
 console.log(toto);
 
-toto= 256%7;
+toto = 256%7;
 console.log(toto);
