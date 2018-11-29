1d51e71f1bb55b40980ead6c6db79bdb1122ec10
Rebecca Dumazert <rebeccadumazert@hotmail.fr>
2018-11-29 13:58:12 +0100

    exo6

---
+++ javascript/lesson3/exercise6/exercise/main.js
@@ -0,0 +1,15 @@
+/* Créer une fonction qui calcule la puissance d'un nombre :
+* Elle prend deux paramètre :
+  * Un nombre
+  * La puissance souhaitée
+* Elle retourne le nombre à la puissance que vous vouliez.
+ */
+
+function puissance1(nombre, puissance) {
+    var res = 1 ; 
+    for ( var i = 0 ; i < puissance ; i++) {
+       res = res * nombre; 
+    }
+    return res;
+}
+console.log(puissance1(10, 6)); 
