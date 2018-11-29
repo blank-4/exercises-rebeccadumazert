fdb893c3477ae88e36c36881eb4e87ec060af6a1
Rebecca Dumazert <rebeccadumazert@hotmail.fr>
2018-11-29 13:58:22 +0100

    exo7

---
+++ javascript/lesson3/exercise7/exercise/main.js
@@ -0,0 +1,16 @@
+/* Créer une fonction qui calcule l'inverse d'un nombre :
+* Elle prend en paramètre le nombre
+* Elle retourne son inverse
+ Créer une fonciton qui calcule l'opposé d'un nombre
+* Elle prend en paramètre le nombre
+* Elle retourne son opposé
+ */
+
+function opposenb(nombre){
+    return nombre * -1;
+}
+console.log(opposenb(3));
+ function inversenb(nombre) {
+    return 1/nombre;
+}
+console.log(inversenb(10));
