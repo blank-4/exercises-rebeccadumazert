8b2584f46df5b894362781073d8e5e7beccaf03b
Rebecca Dumazert <rebeccadumazert@hotmail.fr>
2018-11-29 11:24:11 +0100

    exo4

---
+++ javascript/lesson3/exercise4/exercise/main.js
@@ -0,0 +1,14 @@
+/* Créer une fonction qui prendra deux nombres en paramètres.
+Cette fonction doit retourner le plus grand.
+ */
+
+ function number(nb1, nb2) {
+    if (nb1 > nb2){
+        return nb1;
+    } 
+    else {
+        return nb2;
+    }
+ }
+
+ console.log(number(47, 132));
