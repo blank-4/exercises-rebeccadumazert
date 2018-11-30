7e1388be39df15c2ccf6c0bc238a17820d294104
Rebecca Dumazert <rebeccadumazert@hotmail.fr>
2018-11-30 17:48:23 +0100

    exo 3

---
+++ javascript/lesson4/exercise3/exercise/main.js
@@ -0,0 +1,23 @@
+/* * Créer une fonction qui porte le nom `exercice3`, elle contiendra le code pour les consignes suivantes :
+
+* Déclarer une variable à laquelle vous donnerez le nom de votre choix, elle sera de type objet.
+
+* Donner lui une propriété `date` qui sera une fonction permettant d'afficher la date dans ce format : "nous sommes le 02 01 2018" avec un console.log.
+
+* Appeler la propriété (méthode) pour afficher la date dans la console.
+ */
+
+ function exercice3() {
+     var object = {
+         date : function() {
+            var now = new Date();
+            var date = now.getDate();
+            var month = now.getMonth();
+            var year = now.getFullYear();
+            console.log("Nous sommes le " + date + "/" + month + "/" + year); 
+         }
+    };
+    object.date();     
+ }
+
+ exercice3();
